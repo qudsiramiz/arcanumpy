6524f6572dc47a6056b72b9a1ea79798249ed23a
Ramiz Qudsi <qudsiramiz@gmail.com>
2024-11-25 10:53:09 -0500

    added the brazil_plot_multiple and get_brzl_hist functions

--- arcanum/hist_test.py
+++ arcanum/hist_test.py
@@ -105,7 +105,7 @@ ex, ey, hist, n_hist = hp.hist_plots().brazil_plots(
     pcolormesh_kwargs={"norm": "log", "cmap": "cividis_r"},
 )
 """
-
+"""
 x = np.random.lognormal(mean=0, sigma=2, size=10000)
 x = np.clip(x, 1e-3, 1e2)
 y = np.random.lognormal(mean=0, sigma=2, size=10000)
@@ -131,4 +131,69 @@ axs = hp.hist_plots().kde_plots(
         "xlim": (1e-3, 1e2),
         "ylim": (1e-3, 1e2),
     },
+)
+"""
+
+x = np.random.lognormal(mean=0, sigma=2, size=10000)
+# x = np.clip(x, 1e-3, 1e2)
+y = np.random.lognormal(mean=0, sigma=2, size=10000)
+# y = np.clip(y, 1e-3, 1e2)
+Z1 = np.random.lognormal(mean=0, sigma=2, size=10000)
+# Z1 = np.clip(Z1, 1e-5, 1e0)
+Z2 = np.random.lognormal(mean=0, sigma=2, size=10000)
+# Z2 = np.clip(Z2, 1e-5, 1e0)
+
+indx = np.where((Z1 > 1e-3) & (Z2 > 1e-3))[0]
+z_ratio = Z1 / Z2
+
+z1 = []
+z2 = Z1
+z3 = Z2
+z4 = z_ratio
+
+x_min = 1e-3
+x_max = 1e2
+y_min = 1e-3
+y_max = 1e2
+
+xlim = [x_min, x_max]
+ylim = [y_min, y_max]
+bins = 100
+cmin = 10
+
+(ex2, ey2, hst1, hst2) = hp.hist_plots().get_brzl_hist(
+    x=x, y=y, z=z1, xlim=xlim, ylim=ylim, bins=bins, c_min=cmin
+)
+(ex4, ey4, hst3, hst4) = hp.hist_plots().get_brzl_hist(
+    x=x, y=y, z=z2, xlim=xlim, ylim=ylim, bins=bins, c_min=cmin
+)
+(ex6, ey6, hst5, hst6) = hp.hist_plots().get_brzl_hist(
+    x=x, y=y, z=z3, xlim=xlim, ylim=ylim, bins=bins, c_min=cmin
+)
+(ex8, ey8, hst7, hst8) = hp.hist_plots().get_brzl_hist(
+    x=x, y=y, z=z4, xlim=xlim, ylim=ylim, bins=bins, c_min=cmin
+)
+
+data = [hst2, hst4, hst6, hst8]
+
+ex = ex2
+ey = ey2
+fig = hp.hist_plots().brazil_plot_multiple(
+    data=data,
+    ex=ex,
+    ey=ey,
+    # xmin=1e-3,
+    # xmax=1e2,
+    # ymin=1e-3,
+    # ymax=1e2,
+    # bins=100,
+    # nbins=10,
+    # cmin=10,
+    # cmax=1000,
+    # norm="log",
+    # cmap="cividis_r",
+    # vmin=10,
+    # vmax=1000,
+    save_fig=True,
+    plot_show=False,
 )
